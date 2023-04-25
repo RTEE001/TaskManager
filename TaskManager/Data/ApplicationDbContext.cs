b29cb4428704c04f0ecf7d777637cdcfcc822531
rtee001 <rammbuadthifhidzi@gmail.com>
2023-04-25 18:10:53 +0200

    Project initialisation

--- TaskManager/Data/ApplicationDbContext.cs
+++ TaskManager/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
 
 namespace TaskManager.Data;
 
@@ -9,4 +10,5 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<TaskManager.Models.Job>? Job { get; set; }
 }
